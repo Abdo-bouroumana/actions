d109b8a1a726eac5aa8430be54c80f3e13aa3ff3
abdo <bouroumanaabderrazak@gmail.com>
2026-07-29 17:28:34 +0100

    changes commited

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const assert = require("assert");
 const { add } = require("../src/app.js");
 
 assert.strictEqual(add(2, 3), 5);
-assert.strictEqual(add(-1, 1), 0);
+assert.strictEqual(add(-55, 50), 0);
 
 console.log("✅ All tests passed!");
