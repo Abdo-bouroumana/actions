0996b73b1b239f8f9ac61e72a70e350c83fa191e
Abdo-bouroumana <bouroumanaabderrazak@gmail.com>
2026-08-06 15:05:06 +0100

    Update test case for add function

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const { add } = require("../src/app.js");
 
-assert.strictEqual(add(2, 3), 5);
+assert.strictEqual(add(-2, 7), 5);
 assert.strictEqual(add(-55, 50), 0);
 
 console.log("✅ All tests passed!");
